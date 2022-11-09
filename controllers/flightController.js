8ff2ffa96309592fc45cfce528b81ce937c44dab
Godspower Idiakose <idiakeg@gmail.com>
2022-11-09 12:31:57 +0100

    Created a dummy flight data base to test the get All flights route

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,31 @@
+const date = new Date().toLocaleDateString();
+
+const time = new Date().toLocaleTimeString();
+
+const flights = [
+	{
+		title: "flight to Canada",
+		time,
+		price: new Intl.NumberFormat("en-us", {
+			style: "currency",
+			currency: "NGN",
+		}).format(26000),
+		date,
+	},
+	{
+		title: "flight to Nigeria",
+		time,
+		price: new Intl.NumberFormat("en-us", {
+			style: "currency",
+			currency: "NGN",
+		}).format(56000),
+		date,
+	},
+];
+
 exports.getAllFlights = (req, res) => {
-	console.log("The get all flights route wass visited");
-	res.send("This is where all the available flightrs will be displayed");
+	console.log("The get all flights route was visited");
+	res.send(flights);
 };
 
 exports.frame = "good food";
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -6,6 +6,9 @@ const router = express.Router();
 // --> destruction all of the functions to obtain different routes fron the "flightController"
 const { getAllFlights, frame } = require("../controllers/flightController");
 
+// --> getting all flights
 router.get("/", getAllFlights);
+
+// --> Adding/Booking a Flight
 
 module.exports = router;
